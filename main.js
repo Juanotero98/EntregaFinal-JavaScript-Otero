0a574a3cb06abd8e0cb07c813cb462e91b744391
Juanotero98 <oterojuan1998@gmail.com>
2023-08-02 17:47:43 -0300

    Actualizacion 5

--- main.js
+++ main.js
@@ -1,87 +1,92 @@
 
 ////////-PRIMERA PRE-ENTREGA-////////
 
-//INICIALIZAR CARRITO//
+const carritoCompras = [];
 
-const carritoCompras = []
-
-//FUNCION PARA AGREGAR PRODUCTOS AL CARRITO DE COMPRAS//
-
-function agregaralCarrito(){
-  const nombre = prompt("Ingrese el nombre del producto:")
-  const precio = parseFloat(prompt("Ingrese el precio del producto:"))
-
-  if (isNaN(precio)){
-    alert("El precio ingresado es incorrecto, por favor introdusca otro.")
-    return
-  }
-
-  let producto = {nombre, precio}
-  carritoCompras.push(producto)
-  alert("Producto agregado al carrito de compras")
- 
+//FUNCION PARA AGREGAR AL CARRITO DE COMPRAS//
+function agregarAlCarrito(nombre, precio) {
+  let producto = { nombre, precio };
+  carritoCompras.push(producto);
 }
 
-//FUNCION PARA CALCULAR Y MOSTRAR EL TOTAL DE LA COMPRA//
-
-function calcularTotal(){
-  let total = 0
-  for (let i = 0; i < carritoCompras.length; i++){
-    total += carritoCompras[i].precio
+//FUNCION PARA QUITAR DEL CARRITO DE COMPRAS//
+function quitarDelCarrito(indice) {
+  if (indice >= 0 && indice < carritoCompras.length) {
+    carritoCompras.splice(indice, 1);
+    alert("Producto eliminado del carrito.");
+  } else {
+    alert("Índice inválido. Producto no encontrado en el carrito.");
   }
-  return total
 }
 
-//FUNCION PARA MOSTRAR EL CARRITO DE COMPRAS//
-
-function mostrarCarritoCompras(){
-  let mensaje = "Carrito de compras:/n"
-  for (let i = 0; i < carritoCompras.length; i++){
-   mensaje += `${i + 1}. ${carritoCompras[i].nombre}: $${carritoCompras[i].precio}\n`
+//FUNCION PARA CALCUALR EL TOTAL A GASTAR//
+function calcularTotal() {
+  let total = 0;
+  for (let i = 0; i < carritoCompras.length; i++) {
+    total += carritoCompras[i].precio;
   }
-  const total = calcularTotal()
-  mensaje += ("Total de la compra es: $" + total)
-  alert(mensaje)
+  return total;
 }
 
-//FUNCION PARA QUITAR PRODUCTOS DEL CARRITO DE COMPRAS//
-
-function quitarProducto(){
-  const indice = parseInt(prompt("Ingrese por favor el Indice del producto a eliminar")) - 1
+//FUNCION PARA MOSTRAR CARRITO DE COMPRAS//
+function mostrarCarrito(productos) {
+  alert("Carrito de compra:");
+  for (let i = 0; i < productos.length; i++) {
+    alert(`${i + 1}. ${productos[i].nombre}: $${productos[i].precio}`);
+  }
+  const total = calcularTotal();
+  alert(`Total a gastar: $${total}`);
   
-  if(isNaN(indice) || indice <0 || indice >= carritoCompras.length ){
-    alert("El indice agregado es incorrecto")
-    return
-  }
-
-  carritoCompras.splice(indice, 1)
-  alert("Producto eliminado del carrito de compras")
 }
+
+//FUNCIONES PARA FILTRAR POR PRECIO Y NOMBRE//
+function filtrarPorPrecio(productos, minPrecio, maxPrecio) {
+  return productos.filter(producto => producto.precio >= minPrecio && producto.precio <= maxPrecio);
+}
+
+function filtrarPorNombre(productos, nombre) {
+  return productos.filter(producto => producto.nombre.toLowerCase().includes(nombre.toLowerCase()));
+}
+
+
 
 //CONDICIONES//
+while (true) {
+  const opcion = parseInt(prompt(
+    "Seleccione una opción:\n1. Agregar producto\n2. Quitar producto\n3. Filtrar por precio\n4. Filtrar por nombre\n5. Mostrar carrito\n6. Salir"
+  ));
 
-while(true){
-  const opcion = parseInt(prompt("Seleccione una opcion:/n1. Agregar al carrito:/n2. Mostrar carrito:/n3. Quitar producto:/n4. Salir"))
-
-  switch (opcion){
+  switch (opcion) {
     case 1:
-      agregaralCarrito()
-      break
+      const nombre = prompt("Ingrese el nombre del producto:");
+      const precio = parseFloat(prompt("Ingrese el precio del producto:"));
+      agregarAlCarrito(nombre, precio);
+      break;
     case 2:
-      mostrarCarritoCompras()
-      break
+      const indice = parseInt(prompt("Ingrese el índice del producto a eliminar:")) - 1;
+      quitarDelCarrito(indice);
+      break;
     case 3:
-      quitarProducto()
-      break
+      const minPrecio = parseFloat(prompt("Ingrese el precio mínimo:"));
+      const maxPrecio = parseFloat(prompt("Ingrese el precio máximo:"));
+      mostrarCarrito(filtrarPorPrecio(carritoCompras, minPrecio, maxPrecio));
+      break;
     case 4:
-      alert("Gracias por utilizar el carrito de compras.")
-      break
+      const nombreFiltro = prompt("Ingrese el nombre del producto a filtrar:");
+      mostrarCarrito(filtrarPorNombre(carritoCompras, nombreFiltro));
+      break;
+    case 5:
+      mostrarCarrito(carritoCompras);
+      break;
+    case 6:
+      prompt("Gracias por utilizar el carrito de compras.");
+      break;
     default:
-     alert("Opcion incorrecta")
+      prompt("Opción inválida.");
   }
 
-  if(opcion === 4){
-    break
+  if (opcion === 6) {
+    break;
   }
 }
 
